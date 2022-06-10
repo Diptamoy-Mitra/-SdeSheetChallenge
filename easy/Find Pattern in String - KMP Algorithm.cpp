529e47ed39a42cd31f6a9c50757bddb3e4961686
Diptamoy Mitra <91617575+Diptamoy-Mitra@users.noreply.github.com>
2022-06-10 19:41:13 +0530

    Create Find Pattern in String - KMP Algorithm.cpp

---
+++ easy/Find Pattern in String - KMP Algorithm.cpp
@@ -0,0 +1,37 @@
+bool findPattern(string p, string s)
+
+{ if(p.length()>s.length())return false;
+
+   int j=0;
+
+   for(int i=0;i<s.length();i++){
+
+       if(s[i]==p[j]){
+
+           j++;
+
+       }
+
+       else{
+
+               i=i-j;
+
+           j=0;
+
+       
+
+       }
+
+       if(j==p.length()){
+
+           return true;
+
+           break;
+
+       }
+
+   }
+
+   return false;
+
+}
